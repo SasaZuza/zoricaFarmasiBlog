fdc55a387ba9df5da50278d327e6dd3a0fc6ff3a
‘SasaZuza’ <sasazuzazule@gmail.com>
2019-09-28 11:43:45 +0200

    Adding pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -108,7 +108,7 @@ module.exports.createPages = async ({ graphql, actions }) => {
   const posts = res.data.allContentfulBlogPost.edges
 
   // Create posts pagination pages
-  const postsPerPage = 4
+  const postsPerPage = 3
   const numberOfPages = Math.ceil(posts.length / postsPerPage)
 
   Array.from({ length: numberOfPages }).forEach((_, blog) => {
--- src/templates/post-list.js
+++ src/templates/post-list.js
@@ -6,10 +6,10 @@ import blogStyles from "../pages/blog.module.scss"
 
 const PostList = props => {
   const posts = props.data.allContentfulBlogPost.edges
-  const { currentPage } = props.pageContext
+  // const { currentPage } = props.pageContext
 
   return (
-    <Layout pageTitle={`Page: ${currentPage}`}>
+    <Layout>
       <ol className={blogStyles.posts}>
         {posts.map(({ node }) => (
           <li className={blogStyles.post}>
